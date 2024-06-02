48f49e438ebccb4345823097c33a7b24c978bc41
onury5506 <onuryildizsai@gmail.com>
2024-06-02 16:07:34 +0300

    validation

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Language } from "./translate.dto";
+import { IsEmail, IsEnum, IsNotEmpty } from "class-validator";
 
 export enum UserRoles {
     ADMIN = "admin",
@@ -11,29 +12,36 @@ export class RegisterUserDTO{
         description: "Username of the user",
         example: "onury5506"
     })
+    @IsNotEmpty()
     username: string;
 
     @ApiProperty({
         description: "Name of the user",
         example: "Onur Yıldız"
     })
+    @IsNotEmpty()
     name: string;
 
     @ApiProperty({
         description: "Email of the user",
         example: "example@example.com"
     })
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
 
     @ApiProperty({
         description: "Password of the user",
         example: "password123"
     })
+    @IsNotEmpty()
     password: string;
 
     @ApiProperty({
         description: "Language of the user",
         example: "en"
     })
+    @IsNotEmpty()
+    @IsEnum(Language)
     language: Language;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   const config = new DocumentBuilder()
     .setTitle('Social Media API')
     .setDescription('The Social Media API description')
