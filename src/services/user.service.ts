06035253adcd98b57b835594165d234643fcfc47
onury5506 <onuryildizsai@gmail.com>
2024-06-02 16:53:21 +0300

    Checking username and email is not used before register

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
@@ -11,6 +11,12 @@ export class UserService {
   ) {}
 
   async createUser(user: User): Promise<User> {
+    if(await this.userModel.findOne({ email: user.email })) {
+      throw new HttpException('Email already exists', 400);
+    }else if(await this.userModel.findOne({ username : user.username })) {
+      throw new HttpException('Username already exists', 400);
+    }
+
     user.password = await bcrypt.hash(user.password, 10)
     const newUser = new this.userModel(user);
     return newUser.save();
