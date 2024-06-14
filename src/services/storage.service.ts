60256a2029575c3d05dba6d34fe74b6b12bcfa78
onury5506 <onuryildizsai@gmail.com>
2024-06-14 10:41:31 +0300

    Update StorageService to invalidate cache for a specific path

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,11 +1,18 @@
-import { GetSignedUrlResponse, Storage } from '@google-cloud/storage';
+import { Storage } from '@google-cloud/storage';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Interval } from '@nestjs/schedule';
+import { GoogleAuth } from 'google-auth-library';
+import axios from 'axios';
 
 @Injectable()
 export class StorageService {
     private storage: Storage
     private bucketName: string
+    private auth: GoogleAuth
+    private accessToken: string
+    private urlMap: string
+    private projectId: string
     constructor(
         private readonly configService: ConfigService
     ) {
@@ -14,6 +21,22 @@ export class StorageService {
         })
         
         this.bucketName = this.configService.get<string>("BUCKET_NAME")
+        this.projectId = this.configService.get<string>("GOOGLE_PROJECT_ID")
+        this.urlMap = this.configService.get<string>("GOOGLE_URL_MAP_NAME")
+
+        this.auth = new GoogleAuth({
+            keyFile: this.configService.get<string>("GOOGLE_KEY_FILE_PATH"),
+            scopes: ['https://www.googleapis.com/auth/cloud-platform']
+        })
+
+        this.getClientAccessToken()
+    }
+
+    @Interval(20 * 60 * 1000)
+    private async getClientAccessToken() {
+        const client = await this.auth.getClient()
+        const res = await client.getAccessToken()
+        this.accessToken = res.token
     }
 
     uploadFile(file: Express.Multer.File, path: string): Promise<string> {
@@ -42,5 +65,17 @@ export class StorageService {
         })
 
         return res[0]
+    }
+
+    async invalidateCache(path: string): Promise<void> {
+        const url = `https://compute.googleapis.com/compute/v1/projects/${this.projectId}/global/urlMaps/${this.urlMap}/invalidateCache`
+
+        let res = await axios.post(url, {
+            path
+        }, {
+            headers: {
+                Authorization: `Bearer ${this.accessToken}`
+            }
+        })
     }
 }
