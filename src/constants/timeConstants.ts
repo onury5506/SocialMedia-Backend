8bcb8b56ccb55ef19f7dbfdd4efe8c6918f746ee
onury5506 <onuryildizsai@gmail.com>
2024-06-19 13:42:50 +0300

    Add Time and TimeMs enums to timeConstants

---
+++ src/constants/timeConstants.ts
@@ -0,0 +1,15 @@
+export enum Time {
+    Second = 1,
+    Minute = 60,
+    Hour = 3600,
+    Day = 86400,
+    Week = 604800,
+}
+
+export enum TimeMs {
+    Second = 1000,
+    Minute = 60000,
+    Hour = 3600000,
+    Day = 86400000,
+    Week = 604800000,
+}
