0d02cbefc8c6a5fe32d7d5a12bbc06b3c2e31605
onury5506 <onuryildizsai@gmail.com>
2024-06-19 13:01:26 +0300

    refactor: Add Redis caching to CacheService

---
+++ src/services/cache.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { RedisClientType, createClient } from 'redis';
+
+@Injectable()
+export class CacheService {
+    private client: RedisClientType
+    constructor(
+        private readonly configService: ConfigService
+    ) {
+        this.client = createClient({
+            socket: {
+                host: this.configService.get<string>("REDIS_HOST"),
+                port: this.configService.get<number>("REDIS_PORT")
+            },
+            password: this.configService.get<string>("REDIS_PASSWORD")
+        })
+
+        this.client.on('connect', () => {
+            console.log('Redis client connected');
+        });
+
+        this.client.connect();
+    }
+
+    public async set(key: string, value: any, expire?: number): Promise<any> {
+        return this.client.set(key, JSON.stringify(value), expire ? { EX: expire } : undefined);
+    }
+
+    public async get<T>(key: string): Promise<T | null> {
+        const res = await this.client.get(key)
+
+        return res ? JSON.parse(res) as T : null;
+    }
+
+    public del(key: string): Promise<any> {
+        return this.client.del(key);
+    }
+
+    public isExist(key: string): Promise<boolean> {
+        return this.client.exists(key).then(res => res === 1);
+    }
+}
