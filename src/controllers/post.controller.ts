63d997df872f9b6e8a7d12e13633353309a92250
onury5506 <onuryildizsai@gmail.com>
2024-06-23 00:12:16 +0300

    Develop endpoint for create Post in PostController

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RequestWithUser } from 'src/dto/auth.dto';
+import { CreatePostRequestDto, CreatePostResponseDto } from 'src/dto/post.dto';
+import { JwtGuard } from 'src/guards/jwt.guard';
+import { PostService } from 'src/services/post.service';
 
 @Controller("/post")
 @ApiTags("Post")
 export class PostController {
-    constructor() { }
+    constructor(
+        private readonly postService: PostService,
+    ) { }
+
+    @UseGuards(JwtGuard)
+    @ApiBearerAuth("JwtGuard")
+    @Post("/")
+    async createPost(@Request() req: RequestWithUser,@Body() body: CreatePostRequestDto): Promise<CreatePostResponseDto>{
+        return this.postService.createPost(req.userId, body);
+    }
 }
