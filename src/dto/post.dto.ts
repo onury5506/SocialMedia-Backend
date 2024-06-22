e85a791a56ece08e9e2efcdce9590232dfcee7b0
onury5506 <onuryildizsai@gmail.com>
2024-06-23 00:10:50 +0300

    feat: Add support for post mime type and size in CreatePostRequestDto

--- src/dto/post.dto.ts
+++ src/dto/post.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Size } from "src/constants/fileSizeConstans";
 
 export enum PostType {
     IMAGE = 'image',
@@ -8,11 +9,42 @@ export enum PostType {
 export enum PostStatus {
     INPROGRESS = 'inprogress',
     PUBLISHED = 'published',
+    FAILED = 'failed',
 }
+
+export enum PostMimeType {
+    MP4 = 'video/mp4',
+    MOV = 'video/quicktime',
+    PNG = 'image/png',
+    JPG = 'image/jpeg',
+}
+
+export const MaxPostSizes = {
+    [PostType.IMAGE]: 10 * Size.megabyte,
+    [PostType.VIDEO]: 300 * Size.megabyte,
+}
+
+export const PostMimeTypeToPostType = {
+    [PostMimeType.MP4]: PostType.VIDEO,
+    [PostMimeType.MOV]: PostType.VIDEO,
+    [PostMimeType.PNG]: PostType.IMAGE,
+    [PostMimeType.JPG]: PostType.IMAGE,
+};
 
 export class CreatePostRequestDto {
-    @ApiProperty({ type: 'string', format: 'binary', required: true })
-    file: Express.Multer.File
-
     content: string;
+    postMimeType: PostMimeType;
+    size: number;
 }
+
+export class CreatePostResponseDto {
+    signedUrl: string;
+}
+
+export class VideoTranscodeTaskData {
+    userId: string;
+    postId: string;
+    fileName: string;
+}
+
+export const MaxHashtags = 3;
