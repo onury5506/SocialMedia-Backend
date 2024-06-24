72136ec3a2c4bc2b684d19ba758e17525c11837f
onury5506 <onuryildizsai@gmail.com>
2024-06-24 16:04:28 +0300

    refactor: Update profile picture caching to use Time.Day in UserService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -145,7 +145,7 @@ export class UserService {
       throw new HttpException('findUser.error.userNotFound', 404);
     }
 
-    const profilePicture = res.profilePicture ? await this.storageService.signCdnUrl(res.profilePicture) : "";
+    const profilePicture = res.profilePicture ? await this.storageService.signCdnUrl(res.profilePicture, Time.Day) : "";
     this.cacheService.set(cacheKey, profilePicture, Time.Day).catch(e => { });
 
     return profilePicture;
