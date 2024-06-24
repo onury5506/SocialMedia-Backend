ffc5d6ea36a3d214f7801405770d0b764364f03c
onury5506 <onuryildizsai@gmail.com>
2024-06-24 19:26:26 +0300

    refactor: Add CRUD operations for comments in PostController

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post, UseGuards, Request, Get, Param, HttpException, Delete } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { RequestWithUser } from 'src/dto/auth.dto';
+import { CommentDataWithLikedDto, CreateCommentDto } from 'src/dto/comment.dto';
 import { CreatePostRequestDto, CreatePostResponseDto, PostDataDto, ViewPostDto } from 'src/dto/post.dto';
 import { PostLikeDto, PostUnlikeDto } from 'src/dto/postLike.dto';
 import { JwtGuard } from 'src/guards/jwt.guard';
+import { CommentService } from 'src/services/comment.service';
 import { PostService } from 'src/services/post.service';
 import { PostLikeService } from 'src/services/postLike.service';
 import { UserService } from 'src/services/user.service';
@@ -14,7 +16,7 @@ export class PostController {
     constructor(
         private readonly postService: PostService,
         private readonly postLikeService: PostLikeService,
-        private readonly userService: UserService,
+        private readonly commentService: CommentService,
     ) { }
 
     @UseGuards(JwtGuard)
@@ -60,6 +62,30 @@ export class PostController {
             page = 1;
         }
         return await this.postService.getPostsOfUser(req.userId, userId, page);
+    }
+
+    @UseGuards(JwtGuard)
+    @ApiBearerAuth("JwtGuard")
+    @Get("/comments/:post/:page")
+    async getComments(@Request() req: RequestWithUser, @Param("post") post: string, @Param("page") page: number): Promise<CommentDataWithLikedDto[]>{
+        if(page < 1){
+            page = 1;
+        }
+        return this.commentService.getCommentsOfPost(req.userId, post, page);
+    }
+
+    @UseGuards(JwtGuard)
+    @ApiBearerAuth("JwtGuard")
+    @Post("/comment")
+    commentPost(@Request() req: RequestWithUser, @Body() body: CreateCommentDto): Promise<void>{
+        return this.commentService.createComment(req.userId, body);
+    }
+
+    @UseGuards(JwtGuard)
+    @ApiBearerAuth("JwtGuard")
+    @Delete("/comment/:commentId")
+    deleteComment(@Request() req: RequestWithUser, @Param("commentId") commentId: string): Promise<void>{
+        return this.commentService.deleteComment(req.userId, commentId);
     }
 
     @UseGuards(JwtGuard)
