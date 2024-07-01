69b1ed17eeea64bc19419d618e9bf687530e9697
onury5506 <onuryildizsai@gmail.com>
2024-07-01 12:11:16 +0300

    feat: Add Hashtag schema and model

---
+++ src/schemas/hashtag.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import { User, UserDocument } from "./user.schema";
+import mongoose, { HydratedDocument } from "mongoose";
+
+@Schema({
+    timestamps: true
+})
+export class Hashtag {
+    @Prop()
+    @ApiProperty()
+    name: string;
+
+    @Prop()
+    @ApiProperty()
+    postCount: number;
+
+    @Prop()
+    @ApiProperty()
+    createdAt?: Date;
+
+    @Prop()
+    @ApiProperty()
+    updatedAt?: Date;
+}
+
+export type HashtagDocument = HydratedDocument<Hashtag>
+
+export const HashtagSchema = SchemaFactory.createForClass(Hashtag);
+
+HashtagSchema.index({ name: 1 }, { unique: true });
