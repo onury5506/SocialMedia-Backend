4bfa94530110a52a5967fd4a57b958b389035ac8
onury5506 <onuryildizsai@gmail.com>
2024-07-02 23:28:17 +0300

    refactor: Remove UserModule import from FeedModule

--- src/modules/feed.module.ts
+++ src/modules/feed.module.ts
@@ -4,7 +4,6 @@ import { CacheModule } from './cache.module';
 import { Feed, FeedSchema } from 'src/schemas/feed.schema';
 import { FeedService } from 'src/services/feed.service';
 import { PostModule } from './post.module';
-import { UserModule } from './user.module';
 import { FeedController } from 'src/controllers/feed.controller';
 
 @Module({
@@ -12,7 +11,6 @@ import { FeedController } from 'src/controllers/feed.controller';
         MongooseModule.forFeature([{ name: Feed.name, schema: FeedSchema }]),
         CacheModule,
         PostModule,
-        UserModule,
     ],
     controllers: [FeedController],
     providers: [FeedService],
