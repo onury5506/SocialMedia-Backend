0c5e1926c523f0ac6f5e5e438cb3db19d4187026
onury5506 <onuryildizsai@gmail.com>
2024-07-02 23:02:09 +0300

    refactor: Add Feed schema and model

---
+++ src/schemas/feed.schema.ts
@@ -0,0 +1,50 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import { User, UserDocument } from "./user.schema";
+import mongoose, { HydratedDocument } from "mongoose";
+import { Post, PostDocument } from "./post.schema";
+
+@Schema({
+    timestamps: true
+})
+export class Feed {
+    @Prop({
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User.name
+    })
+    @ApiProperty()
+    owner: UserDocument | mongoose.Types.ObjectId;
+
+    @Prop({
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: Post.name
+    })
+    @ApiProperty()
+    feed: PostDocument[] | mongoose.Types.ObjectId[];
+
+    @Prop()
+    @ApiProperty()
+    lastDateForFollowing?: Date;
+
+    @Prop()
+    @ApiProperty()
+    lastDateForGlobal?: Date;
+
+    @Prop()
+    @ApiProperty()
+    lastDateForFollowingsFollowings?: Date;
+
+    @Prop()
+    @ApiProperty()
+    createdAt?: Date;
+
+    @Prop()
+    @ApiProperty()
+    updatedAt?: Date;
+}
+
+export type FeedDocument = HydratedDocument<Feed>
+
+export const FeedSchema = SchemaFactory.createForClass(Feed);
+
+FeedSchema.index({ owner: 1 }, { unique: true });
