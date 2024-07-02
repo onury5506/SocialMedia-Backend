ef2e917717b6048c7fab7549edb04273249f11c8
onury5506 <onuryildizsai@gmail.com>
2024-07-02 23:01:04 +0300

    refactor: Export PostService from PostModule

--- src/modules/post.module.ts
+++ src/modules/post.module.ts
@@ -37,5 +37,6 @@ import { HashtagModule } from './hashtag.module';
     ],
     controllers: [PostController],
     providers: [PostService, PostLikeService, CommentService, CommentLikeService],
+    exports: [PostService]
 })
 export class PostModule { }
