08bed8b8bc5d823caf21290ca822c19b1b929d35
onury5506 <onuryildizsai@gmail.com>
2024-08-02 20:23:49 +0300

    feat: Add ChatMessage schema and enums for chat message types, status, and room types

--- src/dto/chat.dto.ts
+++ src/dto/chat.dto.ts
@@ -2,3 +2,15 @@ export enum ChatRoomType {
     PRIVATE = 'private',
     GROUP = 'group'
 }
+
+export enum ChatMessageType {
+    TEXT = 'text',
+    IMAGE = 'image',
+    VIDEO = 'video'
+}
+
+export enum ChatMessageStatus {
+    PENDING = 'pending',
+    SENT = 'sent',
+    DELETED = 'deleted'
+}
---
+++ src/schemas/chatMessage.schema.ts
@@ -0,0 +1,56 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import { User, UserDocument } from "./user.schema";
+import mongoose, { HydratedDocument } from "mongoose";
+import { ChatMessageStatus, ChatMessageType, ChatRoomType } from "src/dto/chat.dto";
+import { ChatRoom } from "./chatRoom.schema";
+import { TranslateResultDto } from "src/dto/translate.dto";
+
+@Schema({
+    timestamps: true
+})
+export class ChatMessage {
+    @Prop({
+        type: mongoose.Schema.Types.ObjectId,
+        ref: ChatRoom.name
+    })
+    @ApiProperty()
+    chatRoom:  mongoose.Schema.Types.ObjectId;
+
+    @Prop({
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User.name
+    })
+    @ApiProperty()
+    sender:  mongoose.Schema.Types.ObjectId;
+
+    @Prop()
+    @ApiProperty()
+    messageType: ChatMessageType;
+
+    @Prop()
+    @ApiProperty()
+    messageStatus: ChatMessageStatus;
+
+    @Prop()
+    @ApiProperty()
+    content?: TranslateResultDto;
+
+    @Prop()
+    @ApiProperty()
+    publishedAt: Date;
+
+    @Prop()
+    @ApiProperty()
+    createdAt?: Date;
+
+    @Prop()
+    @ApiProperty()
+    updatedAt?: Date;
+}
+
+export type ChatMessageDocument = HydratedDocument<ChatMessage>
+
+export const ChatMessageSchema = SchemaFactory.createForClass(ChatMessage);
+
+ChatMessageSchema.index({ chatRoom: 1, publishedAt: -1, messageStatus:1 });
